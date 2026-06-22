42573c107f821f05ee3402933bdaa6a0efb7adab
Turilly09 <cebrian.andres@iesutrillas.es>
2026-06-22 17:45:59 +0200

    das

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,11 +1,7 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from 'firebase/app';
+import { getFirestore, initializeFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDBhX68wgQrPXsiIin1sMUTT_LRIYAu8qY",
   authDomain: "gestor-guardias-e36a7.firebaseapp.com",
@@ -16,9 +12,7 @@ const firebaseConfig = {
   measurementId: "G-MV1G295KTP"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const app = initializeApp(firebaseConfig);
 
 export const db = initializeFirestore(app, {
   ignoreUndefinedProperties: true
